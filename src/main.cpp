d185e668d6397032b469fb6ce5f0af807f86cbee
Nite <darkdukey@gmail.com>
2020-03-21 23:55:06 -0700

    Draw Quad

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,17 @@ void processInput(GLFWwindow* window) {
 }
 
 float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-    0.0f, 0.5f, 0.0f};
+    0.5f, 0.5f, 0.0f,    // top right
+    0.5f, -0.5f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f, 0.5f, 0.0f    // top left
+};
+
+unsigned int indices[] = {
+    // note that we start from 0!
+    0, 1, 3,  // first triangle
+    1, 2, 3   // second triangle
+};
 
 const char* vertexShaderSource = R"(
 #version 330 core
@@ -58,8 +66,7 @@ void initShaders(unsigned int& shaderProgram) {
     glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
     if (!success) {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
-             << infoLog << endl;
+        cout << "ERROR: " << infoLog << endl;
     }
 
     // flag shader
@@ -69,8 +76,7 @@ void initShaders(unsigned int& shaderProgram) {
     glCompileShader(fragmentShader);
     if (!success) {
         glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        cout << "ERROR::SHADER::FLAG::COMPILATION_FAILED\n"
-             << infoLog << endl;
+        cout << "ERROR: " << infoLog << endl;
     }
 
     shaderProgram = glCreateProgram();
@@ -80,24 +86,35 @@ void initShaders(unsigned int& shaderProgram) {
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
     if (!success) {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        cout << "ERROR::SHADER::SHADER_PROGRAM_FAILED\n"
-             << infoLog << endl;
+        cout << "ERROR: " << infoLog << endl;
     }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 }
 
 void initBuffers(unsigned int& VAO) {
-    unsigned int VBO;
+    unsigned int VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     // 1. bind Vertex Array Object
     glBindVertexArray(VAO);
     // 2. copy our vertices array in a buffer for OpenGL to use
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     // 3. then set our vertex attributes pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    // Safe to unbind VBO
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // uncomment this call to draw in wireframe polygons.
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 }
 
 int main(int argc, char const* argv[]) {
@@ -136,7 +153,7 @@ int main(int argc, char const* argv[]) {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
